ad28a36189bb4b127ee434a1229e4adbf81fbfa2
h4per <h4peryt@gmail.com>
2023-09-05 19:10:18 +0600

    3

--- apps/todo/admin.py
+++ apps/todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.todo.models import ToDo
 
 # Register your models here.
+@admin.register(ToDo)
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'is_completed', 'created_at', 'image')
